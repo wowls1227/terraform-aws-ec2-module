7c0e7def3742514d6c480cc39f20a268d9356966
wowls1227 <53048187+wowls1227@users.noreply.github.com>
2021-12-07 10:46:34 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "ec2_instance" {
   instance_type          = var.instance_type
   key_name               = var.key_name
 #  monitoring             = true
-#  vpc_security_group_ids = [var.security_group_id]
+  vpc_security_group_ids = [var.security_group_id]
    subnet_id              = var.subnet_id
 
   tags = {
