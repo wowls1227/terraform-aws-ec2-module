03e5fcfe25a83990bc8397036d54684869de5831
wowls1227 <53048187+wowls1227@users.noreply.github.com>
2021-12-07 10:43:48 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region=var.region
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = var.name
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+#  monitoring             = true
+#  vpc_security_group_ids = [var.security_group_id]
+   subnet_id              = var.subnet_id
+
+  tags = {
+    Terraform   = var.tag_terraform
+    Environment = var.tag_env
+  }
+}
